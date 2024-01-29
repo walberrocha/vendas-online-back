8591c092edafcf37f32c091916802f7b03012aec
Walber <walber.rocha.10@gmail.com>
2024-01-29 20:16:18 -0300

    discarte

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,21 @@ import { typeOrmConfig } from './configs/typeorm.config';
 
 @Module({
   imports: [
-    // ConfigModule.forRoot({
-    //   envFilePath: ['.env.development.local'],
-    // }),
-    TypeOrmModule.forRoot(typeOrmConfig),
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
+    TypeOrmModule.forRoot(
+      {
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      type: 'postgres',
+      entities: ['${__dirname}/../**/*.entity{.js,.ts}'],
+      synchronize: true,
+      logging: true,
+    }),
     UserModule,
   ],
   controllers: [],
